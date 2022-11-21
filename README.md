111776083de64665b16a73ae8c3e44527152cccb
QuickPurpleFox <mrocznywilk2001@gmail.com>
2022-11-21 19:14:17 +0100

    Working login and sign up

---
+++ README.md
@@ -0,0 +1,5 @@
+# Studio_Nagran
+Project for college,
+music studio,
+Including: JavaFX, JDBC, MySQL
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module Studio_Nagran {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires java.sql;
+	requires javafx.base;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
