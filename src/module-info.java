97131c9a264d62263d788272df38da4e7f3e6b33
QuickPurpleFox <118301814+QuickPurpleFox@users.noreply.github.com>
2022-12-15 09:46:49 +0100

    Deleted src to migrate to C#

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Studio_Nagran {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires java.sql;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
