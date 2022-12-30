8b85786d4ae734eb526c8118a9f87b6a29d02ede
QuickPurpleFox <mrocznywilk2001@gmail.com>
2022-12-30 20:32:28 +0100

    Moving to MVVM

---
+++ Szprotify/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Windows.Input;
+
+namespace Szprotify.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        //Button LogIn on click
+        LogInCommand = ReactiveCommand.Create(() =>
+        {
+            Console.Write(EntryUsername + " ");
+            Console.WriteLine(EntryPassword);
+        });
+    }
+
+    //Binding from Views to receive input data
+    //https://www.reddit.com/r/AvaloniaUI/comments/zxeua8/cant_get_string_from_textbox_binding/
+    public ICommand LogInCommand {get; }
+    public string EntryUsername {get; set; } = default!;
+    public string EntryPassword {get; set; } = default!;
+    
+}
+
---
+++ Szprotify/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using ReactiveUI;
+
+namespace Szprotify.ViewModels;
+
+/*
+*This is a base class that all ViewModels should inherit. 
+*It implements the way the ViewModels signal changes to the Views to update the UI. 
+*(google INotifyPropertyChanged for more on that.)
+*/
+
+public class ViewModelBase : ReactiveObject
+{
+}
